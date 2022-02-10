18b70b13cb9c8e2bd0ab7c08a65b9c98cba40780
cprice <cprice@tutanota.com>
2022-02-10 22:45:21 +0100

    Can display an isometric map

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
+use bevy::prelude::*;
+use bevy_ecs_tilemap::prelude::*;
+
+mod helpers;
+mod tiled;
+
+fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+
+    let handle: Handle<tiled::TiledMap> = asset_server.load("arena3.tmx");
+
+    let map_entity = commands.spawn().id();
+
+    commands
+        .entity(map_entity)
+        .insert_bundle(tiled::TiledMapBundle {
+            tiled_map: handle,
+            map: Map::new(0u16, map_entity),
+            transform: Transform::from_xyz(0.0, 0.0, 0.0),
+            ..Default::default()
+        });
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(WindowDescriptor {
+            width: 1270.0,
+            height: 720.0,
+            title: String::from("Tiled map editor example"),
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .add_plugin(LogDiagnosticsPlugin::default())
+        .add_plugin(FrameTimeDiagnosticsPlugin::default())
+        .add_plugin(TilemapPlugin)
+        .add_plugin(tiled::TiledMapPlugin)
+        .add_startup_system(startup)
+        .add_system(helpers::camera::movement)
+        .add_system(helpers::texture::set_texture_filters_to_nearest)
+        .run();
 }
